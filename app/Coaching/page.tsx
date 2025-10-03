a66acf186ca573e17c22428a7783120ff4a571b7
venugopalpathuri3000 <pvenugopal2309@gmail.com>
2025-10-03 20:17:51 +0530

    coaching

---
+++ app/Coaching/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <a 
+        href={'/'}
+        > 
+          Go to home
+      </a>
+      coaching
+    </main>
+  );
+}
