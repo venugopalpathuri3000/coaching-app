afb57c37493c429740c561324043ee7b09f534aa
venugopalpathuri3000 <pvenugopal2309@gmail.com>
2025-10-03 19:28:01 +0530

    Initial commit

---
+++ app/components/Card.tsx
@@ -0,0 +1,7 @@
+export default function Card() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Card
+    </main>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Card from './components/Card';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Home
+      <Card />
+    </main>
+  );
+}
