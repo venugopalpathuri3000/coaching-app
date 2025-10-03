382d0436f665c2e9f7149713832187a9d6b3b17a
venugopalpathuri3000 <pvenugopal2309@gmail.com>
2025-10-03 20:00:01 +0530

    feature

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,7 +1,14 @@
-export default function Card() {
+type Props = {
+  header: string;
+  details: string;
+}
+
+export default function Card({header, details} : Props) {
   return (
-    <main className="bg-gray-100 text-black flex flex-col items-center justify-between p-24">
-      Card
+    <main className="w-40 h-72 bg-gray-100 text-black flex flex-col items-center justify-between p-8 m-2">
+      <span className="font-bold">{header}</span>
+      <br/>
+      {details}
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Home
-      <Card />
+      <Card header={"Profile"} details={"Please click on this card to go Profile screen."} />
+      <Card header={"Coaching"} details={"Please click on this card to go Coaching screen."} />
+      <Card header={"FAQ"} details={"Please click on this card to go FAQ screen."} />
     </main>
   );
 }
