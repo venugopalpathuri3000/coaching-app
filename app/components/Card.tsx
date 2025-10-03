cec864acbbfa0d2c1fe6fbe40d252893e38e7732
venugopalpathuri3000 <pvenugopal2309@gmail.com>
2025-10-03 20:06:52 +0530

    feature

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,14 +1,19 @@
 type Props = {
   header: string;
   details: string;
+  link: string;
 }
 
-export default function Card({header, details} : Props) {
+export default function Card({header, details, link} : Props) {
   return (
-    <main className="w-40 h-72 bg-gray-100 text-black flex flex-col items-center justify-between p-8 m-2">
-      <span className="font-bold">{header}</span>
-      <br/>
-      {details}
+    <main >
+      <a 
+        href={link}
+        className="w-40 h-72 bg-gray-100 text-black flex flex-col items-center justify-between p-8 m-2"> 
+          {header}
+          <br/>
+          {details}
+      </a>
     </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Home
-      <Card header={"Profile"} details={"Please click on this card to go Profile screen."} />
-      <Card header={"Coaching"} details={"Please click on this card to go Coaching screen."} />
-      <Card header={"FAQ"} details={"Please click on this card to go FAQ screen."} />
+      <Card header={"Profile"} details={"Please click on this card to go Profile screen."} link={'/profile'}/>
+      <Card header={"Coaching"} details={"Please click on this card to go Coaching screen."} link={'/coaching'} />
+      <Card header={"FAQ"} details={"Please click on this card to go FAQ screen."} link={'/faq'} />
     </main>
   );
 }
